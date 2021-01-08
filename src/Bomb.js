1247378803fd8eaaa8b207b934cede463b1d68b8
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-08 09:53:25 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class Bomb extends React.Component{
+  constructor(props) { 
+  super()
+  this.state = { 
+    someKey: props.someValue
+  }
+}
   render(){
     return(null)
   }
