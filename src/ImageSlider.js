cd1bef6c24ac48f8f7207cbc50c764ba95773866
Bakhos Rachwan <bakhos.rachwan@gmail.com>
2021-01-08 09:45:47 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
 class ImageSlider extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      currentSlideIndex: 0
+    }
+  }
   render(){
     return(null)
   }
